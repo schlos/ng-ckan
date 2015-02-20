6fee85b2829927e301a69877f599b203da41fd21
Rod Wilhelmy <rwilhelmy@gmail.com>
2015-02-20 02:56:27 -0600

    Fetch groups from a remote CKAN

--- app/scripts/services/ckanservice.js
+++ app/scripts/services/ckanservice.js
@@ -11,8 +11,7 @@ angular.module('ngCkanApp')
   .service('ckanService', function ($http) {
     // AngularJS will instantiate a singleton by calling "new" on this function
     var baseUrl = 'http://catalogo.datos.gob.mx/api/3/action/',
-        datasets,
-        dataset;
+        datasets, dataset, groups, group;
 
     function cacheDatasets(response) {
       datasets = response.data.result.results;
@@ -24,6 +23,16 @@ angular.module('ngCkanApp')
       return dataset;
     }
 
+    function cacheGroups(response) {
+      groups = response.data.result;
+      return groups;
+    }
+
+    function cacheGroup(response) {
+      group = response.data.result;
+      return group;
+    }
+
     this.listDatasets = function(start) {
       return $http.get(baseUrl + 'package_search?q=&rows=10&start=' + start).then(cacheDatasets);
     };
@@ -31,5 +40,14 @@ angular.module('ngCkanApp')
     this.showDataset = function(datasetId) {
       return $http.get(baseUrl + 'package_show?id=' + datasetId).then(cacheDataset);
     };
+
+    this.listGroups = function() {
+      return $http.get(baseUrl + 'group_list?all_fields=true').then(cacheGroups);
+    };
+
+    this.showGroup = function(groupId) {
+      return $http.get(baseUrl + 'group_show?id=' + groupId).then(cacheGroup);
+    };
+
   });
 
